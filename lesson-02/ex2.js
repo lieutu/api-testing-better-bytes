09f26ffc8cebe78722cd0bb28f82fe68cc95cc30
Nguyen Lieu Tu <lieutunguyen@Nguyens-Mac-mini.local>
2025-03-11 22:08:48 +0700

    feat: Add lesson 2

---
+++ lesson-02/ex2.js
@@ -0,0 +1,11 @@
+// Khai báo biến
+const myName = "Nguyen Lieu Tu";
+const myAddress = "72 Le Thanh Ton Q1"; 
+const myFavorites = ["listen to music", "watch anime", "travel"]; 
+const isLearningAPI = true;
+
+// In ra m
+console.log("My name is: ", myName);
+console.log("My address is: ", myAddress);
+console.log("My favorites are: ", myFavorites);
+console.log("Am I learning API? ", isLearningAPI);
---
+++ lesson-02/ex3.js
@@ -0,0 +1,9 @@
+let myHeight = 175;
+
+let cannanglytuong = (myHeight - 100) * 0.9;
+let cannangtoida = myHeight - 100;
+let cannangtoithieu = (myHeight - 100)*0.8;
+
+console.log(`Cân nặng lý tưởng: ${cannanglytuong}`);
+console.log(`Cân nặng tối đa: ${cannangtoida}`);
+console.log(`Cân nặng tối thiểu: ${cannangtoithieu}`);
