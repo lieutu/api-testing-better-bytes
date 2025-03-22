1481961d5744cb72b45fdc1c009d3cdaf1970ce2
Nguyen Lieu Tu <lieutunguyen@Nguyens-Mac-mini.local>
2025-03-22 23:01:34 +0700

    feat: Add lesson-04

---
+++ lesson-04/01-setup.js
@@ -0,0 +1,4 @@
+let missionObjective = "Khám phá và kiểm thử API";
+let crew = ["Nam", "Lan", "Hùng"];
+let message = `Hệ thống API Explorer đã sẵn sàng! Phi hành đoàn: ${crew} sẽ cùng bạn thực hiện nhiệm vụ ${missionObjective}!`;
+console.log(message);
---
+++ lesson-04/02-reason.js
@@ -0,0 +1,10 @@
+let apiDefinition =
+  "API là một trung gian phần mềm cho phép hai ứng dụng giao tiếp với nhau";
+let whyTestAPI = [
+  "Đảm bảo tính đúng đắn",
+  "Đảm bảo hiệu suất",
+  "Đảm bảo bảo mật",
+];
+for (let i = 0; i < whyTestAPI.length; i++) {
+  console.log(`Lý do ${i + 1}: ${whyTestAPI[i]}`);
+}
